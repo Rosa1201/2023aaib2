1dad84625f67014c6f9ede3bc99a5ede1dd6d3e9
Rosa1201 <sally20041201@gmail.com>
2024-03-08 11:45:48 +0800

    week03

---
+++ week03/week03-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        T = [0]*101 #統計數字出現次數,因數字1...100,陣列要開101個
+        best = 0 #最多的數字 出現幾次?
+        for num in nums: #迴圈 針對每個數字做處理
+            T[num] += 1 #這個數字num出現次數+1
+            if T[num] > best: best = T[num]#最多出現的T統計數字 是多少
+
+
+        total = 0
+        for t in T:
+            if t==best: total += t
+        return total
