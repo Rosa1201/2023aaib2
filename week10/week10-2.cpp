77fc244e39a0271ae08133d18218348d0c088697
Rosa1201 <sally20041201@gmail.com>
2024-04-26 11:58:10 +0800

    week10

---
+++ week10/week10-2.cpp
@@ -0,0 +1,14 @@
+///a大 b中 c小
+#include <stdio.h>
+int gcd(int a,int b){
+    if(a==0) return b;
+    if(b==0) return a;
+    return gcd(b,a%b);///老二升老大 老三變老二
+}
+int main()
+{
+    printf("請輸入a b兩個數字: ");
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("最大公因數是: %d\n",gcd(a,b));
+}
